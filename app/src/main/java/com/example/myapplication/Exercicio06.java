7108b2f4d1604751e5a664aeb874c9252fd8b864
Leandro <leandro.lanzoni0400@gmail.com>
2025-03-21 17:03:17 -0300

    Exercício 01 e 06

--- app/src/main/java/com/example/myapplication/Exercicio06.java
+++ app/src/main/java/com/example/myapplication/Exercicio06.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,23 @@ public class Exercicio06 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_exercicio06);
+
+        //Declarando os botões
+        Button btnExercicio01 = findViewById(R.id.button1);
+        Button btnExercicio02 = findViewById(R.id.button2);
+        Button btnExercicio03 = findViewById(R.id.button3);
+        Button btnExercicio04 = findViewById(R.id.button4);
+        Button btnExercicio05 = findViewById(R.id.button5);
+        Button btnExercicio06 = findViewById(R.id.button6);
+
+        //Navegando entre as telas
+        btnExercicio01.setOnClickListener(v -> {startActivity(new Intent(this, Exercicio01.class));});
+        btnExercicio02.setOnClickListener(v -> {startActivity(new Intent(this, Exercicio02.class));});
+        btnExercicio03.setOnClickListener(v -> {startActivity(new Intent(this, Exercicio03.class));});
+        btnExercicio04.setOnClickListener(v -> {startActivity(new Intent(this, Exercicio04.class));});
+        btnExercicio05.setOnClickListener(v -> {startActivity(new Intent(this, Exercicio05.class));});
+        btnExercicio06.setOnClickListener(v -> {startActivity(new Intent(this, Exercicio06.class));});
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
